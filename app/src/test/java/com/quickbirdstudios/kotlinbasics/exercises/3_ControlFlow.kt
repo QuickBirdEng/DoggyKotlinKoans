4576ca08a54e51e3fb665869d1cfa2bebfdc3574
Malte Bucksch <malte.bucksch@quickbirdstudios.com>
2017-11-09 10:56:01 +0100

    Correct control flow tests

--- app/src/test/java/com/quickbirdstudios/kotlinbasics/exercises/3_ControlFlow.kt
+++ app/src/test/java/com/quickbirdstudios/kotlinbasics/exercises/3_ControlFlow.kt
@@ -16,17 +16,24 @@ class ControlFlow {
         throw NotImplementedError("Please implement the task :-)")
     }
 
-    //  TODO TASK 2 using the "when" operator, return:
+    //  TODO TASK 2 using the "when" operator on the "dog.age", return:
     //  TODO -> "Baby Dog" if dog is 0 or 1 year old
-    //  TODO -> "Normal Dog" if dog is 2 to 8 years old
+    //  TODO -> "Normal Dog" if dog is 2 to 8 years old (
+    //  HINT: use the range oprator "x..y" and "in" keyword
     //  TODO -> "Old Dog" otherwise
-    //  HINT: use the range oprator "x..y"
-    fun ageToString(dog: Dog): String? {
+    fun ageToString(dog: Dog): String {
         throw NotImplementedError("Please implement the task :-)")
     }
 
     @Test
     fun testOwnerNameIsCorrect() {
+        assertEquals("Baby Dog", ageToString(Dog("Bruno",age = 1)))
+        assertEquals("Normal Dog", ageToString(Dog("Ignatz",age = 5)))
+        assertEquals("Old Dog", ageToString(Dog("Balu",age = 13)))
+    }
+
+    @Test
+    fun testAgeDescriptionIsCorrect() {
         assertEquals("Hans", findDogOwnerName(Dog("Bruno")))
         assertEquals("Peter", findDogOwnerName(Dog("Ignatz")))
         assertEquals(null, findDogOwnerName(Dog("Balu")))
